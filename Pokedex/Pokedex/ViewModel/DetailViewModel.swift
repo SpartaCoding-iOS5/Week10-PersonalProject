30d3e29d9d4a9c1a3218901c3d666e5de62476b2
jamong-mini <jamong.alsl@gmail.com>
2025-01-05 19:43:00 +0900

    ✨[Feat] ViewModel  레이어 구현
    - MainViewModel
        - 포켓몬 목록 데이터 관리 구현
        - 페이지네이션 로직 구현 (20개씩 로딩)
        - 새로고침 기능 추가
    
    - DetailViewModel
        - 포켓몬 상세 정보 조회 구현
        - API 응답 데이터 변환 로직 구현
        - 로딩/에러 상태 관리 추가
    
    - Subject를 사용한 데이터 바인딩
        - BehaviorSubject: 데이터 상태 관리
        - PublishSubject: 에러 처리

---
+++ Pokedex/Pokedex/ViewModel/DetailViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  DetailViewModel.swift
+//  Pokedex
+//
+//  Created by Jamong on 1/5/25.
+//
+
+import Foundation
+import RxSwift
+
+
+class DetailViewModel {
+    // MARK: - Properties
+    
+    /// 구독 해제를 위한 DisposeBag
+    private let disposeBag = DisposeBag()
+    /// 선택된 포켓몬 ID
+    private let pokemonId: Int
+    
+    // MARK: - Subjects
+    
+    /// View가 구독할 Subjects
+    let pokemonSubject = BehaviorSubject<PokemonDetail?>(value: nil)
+    let loadingSubject = BehaviorSubject<Bool>(value: false)
+    let errorSubject = PublishSubject<Error>()
+    
+    // MARK: - Initialization
+    
+    init(pokemonId: Int) {
+        self.pokemonId = pokemonId
+        fetchPokemonDetail()
+    }
+    
+    // MARK: - Public Methods
+    
+    /// 포켓몬 상세 정보를 다시 불러옴
+    func refresh() {
+        fetchPokemonDetail()
+    }
+    
+    /// 포켓몬 상세 정보를 불러오는 함수
+    private func fetchPokemonDetail() {
+        // 로딩 시작
+        loadingSubject.onNext(true)
+        
+        // URL 생성
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonId)") else {
+            errorSubject.onNext(NetworkError.invalidUrl)
+            return
+        }
+        
+        // 네트워크 요청
+        NetworkManager.shared.fetch(url: url)
+            .subscribe(onSuccess: { [weak self] (response: PokemonDetailResponse) in
+                // 포켓몬 모델로 변환
+                guard let imageUrl = URL(string: response.imageUrl) else {
+                    self?.errorSubject.onNext(NetworkError.invalidUrl)
+                    return
+                }
+                
+                let mainType = PokemonType(rawValue: response.mainType)
+                
+                let detail = PokemonDetail(
+                    id: response.id,
+                    name: response.name,
+                    koreanName: "",
+                    imageURL: imageUrl,
+                    height: response.heightInMeters,
+                    weight: response.weightInKg,
+                    mainType: mainType
+                )
+                
+                // 데이터 전달
+                self?.pokemonSubject.onNext(detail)
+                self?.loadingSubject.onNext(false)
+            }, onFailure: { [weak self] error in
+                // 에러 전달
+                self?.errorSubject.onNext(error)
+                self?.loadingSubject.onNext(false)
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
